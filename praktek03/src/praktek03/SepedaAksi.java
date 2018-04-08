fc54fa581e31529dd9f51e53aebe6a463ec8104f
nafisahcaca <imaryani397@gmail.com>
2018-04-08 21:43:25 +0800

    menggunakan method set

--- praktek03/src/praktek03/SepedaAksi.java
+++ praktek03/src/praktek03/SepedaAksi.java
@@ -2,9 +2,10 @@ package praktek03;
 public class SepedaAksi {
     public static void main(String [] args){
         Sepeda s = new Sepeda();
-        s.merk="poligon";
-        s.warna="biru";
-        s.harga=1000;
+        
+        s.setMerk("Butterfly");
+        s.setWarna("Biru");
+        s.setHarga(1000);
 
         s.cetakInfo();
     }
