0f1ddf888d962493bf5de31f0ba963f6949a4206
nafisahcaca <imaryani397@gmail.com>
2018-04-08 21:22:20 +0800

    Instance, buat attribut dan panggil method

--- praktek03/src/praktek03/SepedaAksi.java
+++ praktek03/src/praktek03/SepedaAksi.java
@@ -1,7 +1,12 @@
 package praktek03;
 public class SepedaAksi {
     public static void main(String [] args){
-        
+        Sepeda s = new Sepeda();
+        s.merk="poligon";
+        s.warna="biru";
+        s.harga=1000;
+
+        s.cetakInfo();
     }
     
 }
